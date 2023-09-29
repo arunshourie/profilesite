6095e6918ebcdfd84817bce8114f7dc2c2ad6afc
Neo <me.arunshourie@gmail.com>
2023-09-29 19:26:21 +0530

    Update jasmathur.js

--- jasmathur.js
+++ jasmathur.js
@@ -6,22 +6,6 @@ $(document).ready(function(){
 
 
 
-$('.industry-hover').on('mouseenter mouseleave', function() {
-  $(this).find('.fill').toggleClass('expand');
-  $(this).find('.marquee').toggleClass('showthis');
-});
-
-$('.link-top').on('mouseleave', function() {
-    $(this).closest('.industry-hover').removeClass('bottom');
-    $(this).closest('.industry-hover').find('.marquee').removeClass('bottomalign');
-});
-
-$('.link-bottom').on('mouseleave', function() {
-    $(this).closest('.industry-hover').addClass('bottom');
-    $(this).closest('.industry-hover').find('.marquee').addClass('bottomalign');
-});
-
-
 
 
 
